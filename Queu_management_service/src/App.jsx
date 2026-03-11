083f9e2597c9964b0fa37cfce2618d0a3d63c6ca
adarsh2027dev <adarshtiwaridev01@gmail.com>
2026-03-11 15:56:40 +0530

    Queue management working

--- Queu_management_service/src/App.jsx
+++ Queu_management_service/src/App.jsx
@@ -1,15 +1,23 @@
 import { useState } from 'react'
+import './index.css'
 
-import './App.css'
+ export default function App() 
 
-function App() {
-  const [count, setCount] = useState(0)
+{
 
+  const [count, setCount] = useState(0);
   return (
-  <>
   
-  </>
-  )
+  <div className="App">
+  <header className='text-lg font-black'>Queue Management Service</header>
+  <p>Welcome to the Queue Management Service! This application helps you manage and organize queues efficiently. </p>
+   <div className=" flex flex-row justify-center gap-4 mt-3 justify-items-center">
+    <h1>QueueForm</h1>
+    <h2>Displayform</h2>
+   </div>
+  
+  </div>
+  );
 }
 
-export default App
+
